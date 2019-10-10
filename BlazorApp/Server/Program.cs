0cbc5ed726d511a8eebc79c1ef77aaef42a0dc71
CRS80186 <CRS80186@DESKTOP-9JQTIUV>
2019-10-10 13:54:16 -0400

    migracion hecha

--- BlazorApp/Server/Program.cs
+++ BlazorApp/Server/Program.cs
@@ -14,7 +14,7 @@ namespace BlazorApp.Server
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseConfiguration(new ConfigurationBuilder()
-                    .AddJsonFile("appsetting.json", optional: false, reloadOnChange: true)
+                    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
                     .AddCommandLine(args)
                     .Build())
                 .UseStartup<Startup>()
--- BlazorApp/Shared/Models/Persona.cs
+++ BlazorApp/Shared/Models/Persona.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace BlazorApp.Shared.NewFolder
+namespace BlazorApp.Shared.Models
 {
     public class Persona
     {
